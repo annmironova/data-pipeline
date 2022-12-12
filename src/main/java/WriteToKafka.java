e4a680cab2c02426f623c343755386ccbb24de1e
vogal <vogal.mv@gmail.com>
2022-12-12 19:36:16 +0300

    Write to Kafka as JSON

--- src/main/java/WriteToKafka.java
+++ src/main/java/WriteToKafka.java
@@ -1,12 +1,17 @@
 import org.apache.spark.api.java.function.MapFunction;
 import org.apache.spark.sql.*;
-
+import static org.apache.spark.sql.functions.*;
 
 public class WriteToKafka {
     public static void main(String[] args) {
+
+        String brokers = "localhost:29092";
+        String sourceCsvPath = "src/main/resources/uci-news-aggregator.csv";
+        String topicName = "uci_news";
+
         SparkSession spark = SparkSession
                 .builder()
-                .appName("Java Spark SQL basic example")
+                .appName("Csv to Kafka Json messages")
                 .config("spark.master", "local[2]")
                 .getOrCreate();
 
@@ -14,15 +19,13 @@ public class WriteToKafka {
                 .format("csv")
                 .option("header", "true") //first line in file has headers
                 .option("mode", "DROPMALFORMED")
-                .load("C:/Users/saint/IdeaProjects/data-pipeline/src/main/resources/uci-news-aggregator.csv");
-        df.show();
+                .load(sourceCsvPath);
 
-        Encoder<String> stringEncoder = Encoders.STRING();
-        Dataset<String> df1 = df.map((MapFunction<Row, String>) row -> row.toString(),stringEncoder);
-        df1.printSchema();
+        Dataset<Row> df1 = df.select(col("ID"), to_json(struct("*"))).toDF("key", "value");
+
         df1.write().format("kafka")
-                .option("kafka.bootstrap.servers", "localhost:9092")
-                .option("topic", "dataset")
+                .option("kafka.bootstrap.servers", brokers)
+                .option("topic", topicName)
                 .save();
 
     }
