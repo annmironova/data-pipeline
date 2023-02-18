fc4bce4f71c66d59d06c82d8e89092e4381376d1
vogal <vogal.mv@gmail.com>
2023-02-18 18:15:02 +0300

    Remove table creation as it is done in the app itself

--- src/main/resources/sql/recreate_tables.sql
+++ src/main/resources/sql/recreate_tables.sql
@@ -1,13 +1,2 @@
 drop schema if exists test;
 create schema if not exists test;
-
-create table test.uc_news (
-                         ID int,
-                         TITLE varchar(256),
-                         URL varchar(256),
-                         PUBLISHER varchar(256),
-                         CATEGORY varchar(256),
-                         STORY varchar(256),
-                         HOSTNAME varchar(256),
-                         ts timestamp
-);
